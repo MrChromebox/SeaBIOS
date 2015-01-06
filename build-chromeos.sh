f836d7397e7e63e38760c16aa53539e1de784ae9
Matt DeVillier <matt.devillier@gmail.com>
2015-01-06 14:32:42 -0600

    build-chromeos: use non-headless VGA BIOS
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- build-chromeos.sh
+++ build-chromeos.sh
@@ -7,7 +7,7 @@ if [ $? -eq 0 ]; then
 filename="seabios-haswell-`date +"%Y%m%d"`-md.bin"
 cp seabios-empty.bin ${filename}
 cbfstool ${filename} add-payload -f ./out/bios.bin.elf -n payload -b 0x0
-cbfstool ${filename} add -f ~/coreboot/3rdparty/mainboard/google/panther/vgabios.bin -n pci8086,0406.rom -t optionrom
+cbfstool ${filename} add -f ~/coreboot/3rdparty/mainboard/google/panther/vgabios_orig.bin -n pci8086,0406.rom -t optionrom
 cbfstool ${filename} add -f ~/coreboot/cbfs/boot-menu-wait -n etc/boot-menu-wait -t raw
 cbfstool ${filename} add -f ~/coreboot/cbfs/boot-menu-key -n etc/boot-menu-key -t raw
 cbfstool ${filename} add -f ~/coreboot/cbfs/boot-menu-message -n etc/boot-menu-message -t raw
