31b504bb726b29838ebabae627a519b4dc977bba
Matt DeVillier <matt.devillier@gmail.com>
2015-01-23 12:46:20 -0600

    build-chromeos: create separate payload for Chromebooks w/proper VGA BIOS
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- build-chromeos.sh
+++ build-chromeos.sh
@@ -4,7 +4,8 @@ rm -rf ./out
 cp .config-chromeos .config
 make
 if [ $? -eq 0 ]; then
-filename="seabios-haswell-`date +"%Y%m%d"`-md.bin"
+filename="seabios-haswell-box-`date +"%Y%m%d"`-md.bin"
+filename2="seabios-haswell-book-`date +"%Y%m%d"`-md.bin"
 cp seabios-empty.bin ${filename}
 cbfstool ${filename} add-payload -f ./out/bios.bin.elf -n payload -b 0x0
 cbfstool ${filename} add -f ~/coreboot/3rdparty/mainboard/google/panther/vgabios_orig.bin -n pci8086,0406.rom -t optionrom
@@ -14,4 +15,13 @@ cbfstool ${filename} add -f ~/coreboot/cbfs/boot-menu-message -n etc/boot-menu-m
 cbfstool ${filename} add -f ~/coreboot/cbfs/links -n links -t raw
 cbfstool ${filename} print
 md5sum ${filename} > ${filename}.md5
+cp seabios-empty.bin ${filename2}
+cbfstool ${filename2} add-payload -f ./out/bios.bin.elf -n payload -b 0x0
+cbfstool ${filename2} add -f ~/coreboot/3rdparty/mainboard/google/peppy/vgabios.bin -n pci8086,0406.rom -t optionrom
+cbfstool ${filename2} add -f ~/coreboot/cbfs/boot-menu-wait -n etc/boot-menu-wait -t raw
+cbfstool ${filename2} add -f ~/coreboot/cbfs/boot-menu-key -n etc/boot-menu-key -t raw
+cbfstool ${filename2} add -f ~/coreboot/cbfs/boot-menu-message -n etc/boot-menu-message -t raw
+cbfstool ${filename2} add -f ~/coreboot/cbfs/links -n links -t raw
+cbfstool ${filename2} print
+md5sum ${filename2} > ${filename2}.md5
 fi
