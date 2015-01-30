cf76e9be6fdfdfcdadd31033de2d33e13de68427
Matt DeVillier <matt.devillier@gmail.com>
2015-01-29 18:33:54 -0600

    build-chromeos: use updated HSW/BDW VGA BIOS
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- build-chromeos.sh
+++ build-chromeos.sh
@@ -8,7 +8,7 @@ filename="seabios-haswell-box-`date +"%Y%m%d"`-md.bin"
 filename2="seabios-haswell-book-`date +"%Y%m%d"`-md.bin"
 cp seabios-empty.bin ${filename}
 cbfstool ${filename} add-payload -f ./out/bios.bin.elf -n payload -b 0x0
-cbfstool ${filename} add -f ~/coreboot/3rdparty/mainboard/google/panther/vgabios_orig.bin -n pci8086,0406.rom -t optionrom
+cbfstool ${filename} add -f ~/coreboot/3rdparty/mainboard/google/panther/hsw_1035_cbox.dat -n pci8086,0406.rom -t optionrom
 cbfstool ${filename} add -f ~/coreboot/cbfs/boot-menu-wait -n etc/boot-menu-wait -t raw
 cbfstool ${filename} add -f ~/coreboot/cbfs/boot-menu-key -n etc/boot-menu-key -t raw
 cbfstool ${filename} add -f ~/coreboot/cbfs/boot-menu-message -n etc/boot-menu-message -t raw
@@ -17,7 +17,7 @@ cbfstool ${filename} print
 md5sum ${filename} > ${filename}.md5
 cp seabios-empty.bin ${filename2}
 cbfstool ${filename2} add-payload -f ./out/bios.bin.elf -n payload -b 0x0
-cbfstool ${filename2} add -f ~/coreboot/3rdparty/mainboard/google/peppy/vgabios.bin -n pci8086,0406.rom -t optionrom
+cbfstool ${filename2} add -f ~/coreboot/3rdparty/mainboard/google//panther/hsw_1035_cbook.dat -n pci8086,0406.rom -t optionrom
 cbfstool ${filename2} add -f ~/coreboot/cbfs/boot-menu-wait -n etc/boot-menu-wait -t raw
 cbfstool ${filename2} add -f ~/coreboot/cbfs/boot-menu-key -n etc/boot-menu-key -t raw
 cbfstool ${filename2} add -f ~/coreboot/cbfs/boot-menu-message -n etc/boot-menu-message -t raw
